4026b0222a5abbae8ae5517f0d10f518c2fb0c0a
Sumanth <60640082+Sumanth-Guduru@users.noreply.github.com>
2024-07-25 13:19:00 -0400

    made it responsive

--- src/app/Ui/Sidebar.tsx
+++ src/app/Ui/Sidebar.tsx
@@ -1,7 +1,7 @@
 "use client"
-import React from 'react';
+import React, { useState } from 'react';
 import * as Icons from 'react-icons/ri'; 
-import { RiHomeSmile2Line } from "react-icons/ri";
+import { RiHomeSmile2Line, RiMenuLine, RiCloseLine } from "react-icons/ri";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { PiFolderBold } from "react-icons/pi";
 import { FiTrash2 } from "react-icons/fi";
@@ -14,73 +14,92 @@ import { usePathname } from 'next/navigation';
 import { BsToggle2On } from "react-icons/bs";
 import { LiaToggleOnSolid } from "react-icons/lia";
 
-
 const Sidebar = () => {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarVisible(!isSidebarVisible);
+  };
 
   return (
-    <div className="h-screen bg-customGray text-white p-4 flex flex-col ">
-     <div className="flex justify-between items-center mb-8">
-  <div className="flex items-center">
-    <div className="w-8 h-8 rounded-md bg-blue-500 flex items-center justify-center">
-      <LiaToggleOnSolid className='text-lg text-slate-200'/>
-    </div>
-    <span className="ml-2 text-lg font-bold ">Untitled UI</span>
-  </div>
-  <span className="flex items-center text-gray-500 text-sm">
-    v4.0
-    <FaExternalLinkAlt className="inline-block ml-2 text-gray-500" />
-  </span>
-</div>
-
-      <nav className="space-y-0">
-      <a href="./" className="flex items-center space-x-2 hover:bg-backgroundSoft p-2 rounded-md text-sm transition-colors duration-300 ease-in-out">
-         
-          <span> <RiHomeSmile2Line className="inline-block mr-2 mb-1 text-2xl text-customSliver" />Home</span>
-        </a>
-        <a href="#" className="flex items-center space-x-2 hover:bg-customHovercolour p-2 rounded-md text-sm">
-          <span><PiFolderBold className='inline-block mr-2 mb-1 text-2xl text-customSliver' />All files</span>
-        </a>
-        <a href="#" className="flex items-center space-x-2 hover:bg-customHovercolour p-2 rounded-md text-sm">
-          <span><PiFolderLock className='inline-block mr-2 mb-1 text-2xl text-customSliver' />Private files</span>
-        </a>
-        <a href="#" className="flex items-center space-x-2 hover:bg-customHovercolour p-2 rounded-md text-sm">
-          <span><TbUsersPlus className='inline-block mr-2 mb-1 text-2xl text-customSliver' />Shared with me</span>
-        </a>
-        <a href="#" className="flex items-center space-x-2 hover:bg-customHovercolour p-2 rounded-md text-sm">
-          <span><FiTrash2 className='inline-block mr-2 mb-1 text-2xl text-customSliver' />Deleted files</span>
-        </a>
-        <a href="#" className="flex items-center space-x-2 hover:bg-customHovercolour p-2 rounded-md text-sm">
-          <span><FaCircleHalfStroke className='inline-block mr-2 mb-1 text-2xl text-customSliver' />Design</span>
-        </a>
-        <a href="#" className="flex items-center justify-between space-x-2 hover:bg-customHovercolour p-2 rounded-md text-sm">
-  <span className="flex items-center">
-    <PiNotificationBold className='inline-block mr-2 mb-1 text-2xl text-customSliver' />
-    Notifications
-  </span>
-  <div className='flex items-center justify-center rounded-full w-5 h-5 bg-blue-500 text-white text-xs'>
-    8
-  </div>
-</a>
-        <a href="/settings" className="flex items-center space-x-2 hover:bg-customHovercolour p-2 rounded-md text-sm">
-          <span><IoSettingsOutline className='inline-block mr-2 mb-1 text-2xl text-customSliver' />Settings</span>
-        </a>
-      </nav>
-      <div className="mt-auto  bg-CustomGray2  rounded-md  overflow-hidden">
-        <div className="pt-2 px-2 ">
-          <div className="flex justify-between items-center mb-2">
-            <div className="text-sm text-slate-200">Storage</div>
-            <button className=" text-slate-200 px-4  rounded-md text-sm underline">Upgrade</button>
-          </div>
-          <div className="flex items-center space-x-2 mb-2">
-            <div className="w-full bg-gray-600 h-2 rounded-full overflow-hidden">
-              <div className="bg-slate-100 h-full" style={{ width: '92%' }}></div>
+    <div className="flex h-screen">
+      <div className={`fixed inset-0 z-20 bg-black opacity-50 transition-opacity md:hidden ${isSidebarVisible ? 'block' : 'hidden'}`} onClick={toggleSidebar}></div>
+      <div className={`fixed inset-y-0 left-0 z-30 w-64 bg-customGray text-white p-4 flex flex-col transition-transform transform md:relative md:translate-x-0 ${isSidebarVisible ? 'translate-x-0' : '-translate-x-full'}`}>
+        <div className="flex justify-between items-center mb-8">
+          <div className="flex items-center">
+            <div className="w-8 h-8 rounded-md bg-blue-500 flex items-center justify-center">
+              <LiaToggleOnSolid className='text-lg text-slate-200' />
             </div>
-           
+            <span className="ml-2 text-lg font-bold">Untitled UI</span>
           </div>
-          <div className="flex justify-between items-center">
-            <div className="text-sm text-gray-400 ">9.2 GB of 10 GB used</div>
-            </div>
+          <span className="flex items-center text-gray-500 text-sm">
+            v4.0
+            <FaExternalLinkAlt className="inline-block ml-2 text-gray-500" />
+          </span>
         </div>
+        
+        <nav className="space-y-0">
+          <a href="./" className="flex items-center space-x-2 hover:bg-backgroundSoft p-2 rounded-md text-sm transition-colors duration-300 ease-in-out">
+            <RiHomeSmile2Line className="inline-block mr-2 mb-1 text-2xl text-customSliver" />
+            <span>Home</span>
+          </a>
+          <a href="#" className="flex items-center space-x-2 hover:bg-customHovercolour p-2 rounded-md text-sm">
+            <PiFolderBold className='inline-block mr-2 mb-1 text-2xl text-customSliver' />
+            <span>All files</span>
+          </a>
+          <a href="#" className="flex items-center space-x-2 hover:bg-customHovercolour p-2 rounded-md text-sm">
+            <PiFolderLock className='inline-block mr-2 mb-1 text-2xl text-customSliver' />
+            <span>Private files</span>
+          </a>
+          <a href="#" className="flex items-center space-x-2 hover:bg-customHovercolour p-2 rounded-md text-sm">
+            <TbUsersPlus className='inline-block mr-2 mb-1 text-2xl text-customSliver' />
+            <span>Shared with me</span>
+          </a>
+          <a href="#" className="flex items-center space-x-2 hover:bg-customHovercolour p-2 rounded-md text-sm">
+            <FiTrash2 className='inline-block mr-2 mb-1 text-2xl text-customSliver' />
+            <span>Deleted files</span>
+          </a>
+          <a href="#" className="flex items-center space-x-2 hover:bg-customHovercolour p-2 rounded-md text-sm">
+            <FaCircleHalfStroke className='inline-block mr-2 mb-1 text-2xl text-customSliver' />
+            <span>Design</span>
+          </a>
+          <a href="#" className="flex items-center justify-between space-x-2 hover:bg-customHovercolour p-2 rounded-md text-sm">
+            <span className="flex items-center">
+              <PiNotificationBold className='inline-block mr-2 mb-1 text-2xl text-customSliver' />
+              Notifications
+            </span>
+            <div className='flex items-center justify-center rounded-full w-5 h-5 bg-blue-500 text-white text-xs'>
+              8
+            </div>
+          </a>
+          <a href="/settings" className="flex items-center space-x-2 hover:bg-customHovercolour p-2 rounded-md text-sm">
+            <IoSettingsOutline className='inline-block mr-2 mb-1 text-2xl text-customSliver' />
+            <span>Settings</span>
+          </a>
+        </nav>
+        
+        <div className="mt-auto bg-CustomGray2 rounded-md overflow-hidden">
+          <div className="pt-2 px-2">
+            <div className="flex justify-between items-center mb-2">
+              <div className="text-sm text-slate-200">Storage</div>
+              <button className="text-slate-200 px-4 rounded-md text-sm underline">Upgrade</button>
+            </div>
+            <div className="flex items-center space-x-2 mb-2">
+              <div className="w-full bg-gray-600 h-2 rounded-full overflow-hidden">
+                <div className="bg-slate-100 h-full" style={{ width: '92%' }}></div>
+              </div>
+            </div>
+            <div className="flex justify-between items-center">
+              <div className="text-sm text-gray-400">9.2 GB of 10 GB used</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div className="md:hidden  fixed top-4 left-4 z-40">
+        <button onClick={toggleSidebar}>
+          {isSidebarVisible ? <RiCloseLine className="text-2xl text-white" /> : <RiMenuLine className="text-2xl text-white" />}
+        </button>
       </div>
     </div>
   );
--- src/app/Ui/settings.tsx
+++ src/app/Ui/settings.tsx
@@ -8,10 +8,10 @@ import IOSSwitch from '../Ui/iosSwitch';
 import Braveicon from "../assets/brave-icon.svg";
 import Appleicon from "../assets/Apple_logo_white.svg.png";
 import CircularProgress from "../Ui/progressBar";
-import Mexicoflag from "../assets/Flag_of_Mexico.svg"
-import Vietnamflag from "../assets/Flag_of_Vietnam.svg.png"
+import Mexicoflag from "../assets/Flag_of_Mexico.svg";
+import Vietnamflag from "../assets/Flag_of_Vietnam.svg.png";
 import Instatick from "../assets/insta-tick.svg";
-import sumanth from "../assets/sumanth.jpg"
+import sumanth from "../assets/sumanth.jpg";
 
 const Settings = () => {
   return (
@@ -26,18 +26,17 @@ const Settings = () => {
             <input
               type="text"
               placeholder="Search"
-              className="bg-customGray pl-8 pr-2 py-1 rounded-md text-sm"
+              className="bg-customGray pl-8 pr-2 py-1 rounded-md text-sm  "
             />
-            <CiSearch className="absolute left-2 top-1/2 transform -translate-y-1/2" />
+            <CiSearch className="absolute left-2 top-1/2 transform -translate-y-1/2 " />
           </div>
           <button className="bg-customGray text-white py-1 px-2 rounded-md text-sm flex items-center">
             <FiPlus className="mr-1" /> Invite
           </button>
           <button className="bg-blue-600 text-white py-1 px-2 rounded-md text-sm flex items-center">Upgrade</button>
-          <div className="flex items-center relative">
-            <Image src={sumanth}alt="Profile" className="rounded-full w-7 h-7 mr-2 object-cover" />
-            <Image alt='bluetick'src={Instatick} className='absolute w-3 h-3 bottom-0 left-5 top-4'/>
-            
+          <div className=" items-center relative hidden md:flex">
+            <Image src={sumanth} alt="Profile" className="rounded-full w-7 h-7 mr-2 object-cover" />
+            <Image alt='bluetick' src={Instatick} className='absolute w-3 h-3 bottom-0 left-5 top-4'/>
           </div>
         </div>
       </div>
@@ -53,109 +52,116 @@ const Settings = () => {
         <a href="#" className="text-gray-400 hover:text-white">Sharing</a>
       </div>
 
-      <div className="bg-customGray  p-4 rounded-md mb-4 text-sm">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="mb-4 md:mb-0">
-            <div className="relative flex items-center">
-              <CircularProgress size={30} strokeWidth={5} percentage={80}  />
-              <h2 className="text-sm font-bold ml-2">Your account security is 90%</h2>
+      <div className="flex-grow overflow-y-auto pr-2">
+        <div className="bg-customGray p-4 rounded-md mb-4 text-sm">
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className="mb-4 md:mb-0">
+              <div className="relative flex items-center">
+                <CircularProgress size={30} strokeWidth={5} percentage={80} />
+                <h2 className="text-sm font-bold ml-2">Your account security is 90%</h2>
+              </div>
+              <p className="text-gray-400">Please review and update your password regularly.</p>
             </div>
-            <p className="text-gray-400">Please review and update your password regularly.</p>
+            <div className="flex space-x-2">
+              <button className="bg-CustomGray2 text-gray-400 py-1 px-2 rounded-md text-sm">Dismiss</button>
+              <button className="bg-blue-600 py-1 px-2 rounded-md text-sm">Review Security</button>
+            </div>
           </div>
-          <div className="flex space-x-2">
-            <button className="bg-CustomGray2 text-gray-400 py-1 px-2 rounded-md text-sm">Dismiss</button>
-            <button className="bg-blue-600 py-1 px-2 rounded-md text-sm">Review Security</button>
-          </div>
-        </div>
-      </div>
-
-      <div className="mb-4">
-        <h3 className="text-md font-bold mb-2">Basics</h3>
-        <div className="border-t-2 border-customGray p-3 mb-2 flex flex-col md:flex-row items-center text-sm">
-          <div className="flex-1 space-y-2">
-            <p className="text-gray-400">Password</p>
-            <p className='text-white'>Set a password to protect your account</p>
-          </div>
-          <div className="flex items-center space-x-2 mr-72">
-            <p>●●●●●●●●●●●●●●●●</p>
-            <span className="text-green-500"><FaRegCircleCheck className='text-green-500 inline-block mr-1' />Very secure</span>
-          </div>
-          <button className="bg-CustomGray2 py-1 px-2 rounded-md text-sm">Edit</button>
         </div>
 
-        <div className="border-t-2 border-b-2 border-customGray p-3 rounded-md flex flex-col md:flex-row justify-between items-center text-sm">
-          <div className="flex-1">
-            <p className="text-gray-400">Two-step verification</p>
-            <p>We recommend requiring a verification</p>
-              <p> code in addition to your password.</p>
-          </div>
-          <div className="flex items-center space-x-2 mr-80 ">
-            <IOSSwitch className="mr-2" />
-            <span>Two-Step verification</span>
-          </div>
-          <button className="bg-CustomGray2  py-1 px-2 rounded-md text-sm">Edit</button>
-        </div>
-      </div>
-
-      <div className=''>
-        <h3 className="text-md font-bold mb-2">Browsers and devices</h3>
-        <p className="text-gray-400 mb-2 text-sm">These browsers and devices are currently signed in. Remove unauthorized devices.</p>
-        
-        <div className="border-t-2 border-CustomGray2 p-2 mb-2 flex flex-col md:flex-row justify-between items-center text-sm">
-          <div className="flex-1 flex items-center">
-            <div className="p-2 bg-CustomGray2 rounded-md mr-2">
-              <Image src={Braveicon} alt='' className="w-4 h-4" />
+        <div className="mb-4">
+          <h3 className="text-md font-bold mb-2">Basics</h3>
+          <div className="border-t-2 border-customGray p-3 mb-2 flex flex-col md:flex-row items-center text-sm">
+            <div className="flex-1 space-y-2">
+              <p className="text-gray-400">Password</p>
+              <p className='text-white'>Set a password to protect your account</p>
             </div>
-            <p className="text-white">Brave on Mac OS X</p>
+            <div className="flex items-center space-x-2 md:mr-72">
+              <p>●●●●●●●●●●●●●●●●</p>
+              <span className="text-green-500"><FaRegCircleCheck className='text-green-500 inline-block mr-1' />Very secure</span>
+            </div>
+            <button className="bg-CustomGray2 py-1 px-2 rounded-md text-sm">Edit</button>
           </div>
-          <div className="flex-1 md:flex-none">
-            <p className="text-gray-400 mr-16"><Image src={Vietnamflag} alt="flag" className="rounded-full w-4 h-4 mr-2 inline-block" />Ninh Binh, Vietnam&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Current session</p>
+
+          <div className="border-t-2 border-b-2 border-customGray p-3 rounded-md flex flex-col md:flex-row justify-between items-center text-sm">
+            <div className="flex-1">
+              <p className="text-gray-400">Two-step verification</p>
+              <p>We recommend requiring a verification code in addition to your password.</p>
+            </div>
+            <div className="flex items-center space-x-2 md:mr-80">
+              <IOSSwitch className="mr-2" />
+              <span>Two-Step verification</span>
+            </div>
+            <button className="bg-CustomGray2 py-1 px-2 rounded-md text-sm">Edit</button>
           </div>
-          <FiTrash2 className='text-lg text-gray-600'/>
         </div>
 
-        <div className="border-t-2 border-CustomGray2 p-2 mb-2 flex flex-col md:flex-row justify-between items-center text-sm">
-          <div className="flex-1 flex items-center">
-            <div className="p-2 bg-CustomGray2 rounded-md mr-2">
-              <Image src={Appleicon} alt='' className="w-4 h-4" />
+        <div className=''>
+          <h3 className="text-md font-bold mb-2">Browsers and devices</h3>
+          <p className="text-gray-400 mb-2 text-sm">These browsers and devices are currently signed in. Remove unauthorized devices.</p>
+          
+          <div className="border-t-2 border-CustomGray2 p-2 mb-2 flex flex-col md:flex-row justify-between items-center text-sm">
+            <div className="flex-1 flex items-center">
+              <div className="p-2 bg-CustomGray2 rounded-md mr-2">
+                <Image src={Braveicon} alt='' className="w-4 h-4" />
+              </div>
+              <p className="text-white">Brave on Mac OS X</p>
             </div>
-            <p className="text-white">Mia's MacBook Pro</p>
-          </div>
-          <div className="flex-1 md:flex-none">
-            <p className="text-gray-400 mr-16"><Image src={Vietnamflag} alt="flag" className="rounded-full w-4 h-4 mr-2 inline-block" />Ninh Binh, Vietnam&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Current session</p>
-          </div>
-          <FiTrash2 className='text-lg text-gray-600'/>
-        </div>
-        
-        <div className="border-t-2 border-CustomGray2 p-2 mb-2 flex flex-col md:flex-row justify-between items-center text-sm">
-          <div className="flex-1 flex items-center">
-            <div className="p-2 bg-CustomGray2 rounded-md mr-2">
-              <Image src={Braveicon} alt='' className="w-4 h-4" />
+            <div className="flex-1 md:flex-none flex items-center justify-center md:justify-start md:mr-16">
+              <Image src={Vietnamflag} alt="flag" className="rounded-full w-4 h-4 mr-2 inline-block" />
+              <p className="text-gray-400">Ninh Binh, Vietnam&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Current session</p>
+              <FiTrash2 className='text-lg text-gray-600 ml-6 md:ml-12'/>
             </div>
-            <p className="text-white">Brave on Windows</p>
+           
           </div>
-          <div className="flex-1 md:flex-none">
-            <p className="text-gray-400 mr-20"><Image src={Mexicoflag}  alt="flag" className="rounded-full w-4 h-4 mr-2 inline-block" />Mexico city, Mexico&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 month ago</p>
-          </div>
-          <FiTrash2 className='text-lg text-gray-600'/>
-        </div>
-           <div className="border-t-2 border-b-2  border-CustomGray2 p-2 mb-2 flex flex-col md:flex-row justify-between items-center text-sm">
-          <div className="flex-1 flex items-center">
-            <div className="p-2 bg-CustomGray2 rounded-md mr-2">
-              <Image src={Appleicon} alt='' className="w-4 h-4" />
+
+          <div className="border-t-2 border-CustomGray2 p-2 mb-2 flex flex-col md:flex-row justify-between items-center text-sm">
+            <div className="flex-1 flex items-center">
+              <div className="p-2 bg-CustomGray2 rounded-md mr-2">
+                <Image src={Appleicon} alt='' className="w-4 h-4" />
+              </div>
+              <p className="text-white">Mia's MacBook Pro</p>
             </div>
-            <p className="text-white">Mia's MacBook Pro</p>
-          </div>
-          <div className="flex-1 md:flex-none">
+            <div className="flex-1 md:flex-none flex items-center justify-center md:justify-start md:mr-16">
+              <Image src={Vietnamflag} alt="flag" className="rounded-full w-4 h-4 mr-2 inline-block" />
+              <p className="text-gray-400">Ninh Binh, Vietnam&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Current session</p>
+              <FiTrash2 className='text-lg text-gray-600 ml-6 md:ml-12'/>
+            </div>
             
-            <p className="text-gray-400 mr-20 ">  <Image src={Mexicoflag} alt="flag" className="rounded-full w-4 h-4 mr-2 inline-block" />Mexico city, Mexico&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 month ago</p>
           </div>
-          <FiTrash2 className='text-lg text-gray-600'/>
+          
+          <div className="border-t-2 border-CustomGray2 p-2 mb-2 flex flex-col md:flex-row justify-between items-center text-sm">
+            <div className="flex-1 flex items-center">
+              <div className="p-2 bg-CustomGray2 rounded-md mr-2">
+                <Image src={Braveicon} alt='' className="w-4 h-4" />
+              </div>
+              <p className="text-white">Brave on Windows</p>
+            </div>
+            <div className="flex-1 md:flex-none flex items-center justify-center md:justify-start md:mr-16">
+              <Image src={Mexicoflag} alt="flag" className="rounded-full w-4 h-4 mr-2 inline-block" />
+              <p className="text-gray-400">Mexico City, Mexico&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 month ago&nbsp;&nbsp;&nbsp;&nbsp;</p>
+              <FiTrash2 className='text-lg text-gray-600 ml-6 md:ml-12'/>
+            </div>
+         
+          </div>
+          <div className="border-t-2 border-CustomGray2 p-2 mb-2 flex flex-col md:flex-row justify-between items-center text-sm">
+            <div className="flex-1 flex items-center">
+              <div className="p-2 bg-CustomGray2 rounded-md mr-2">
+                <Image src={Appleicon} alt='' className="w-4 h-4" />
+              </div>
+              <p className="text-white">Mia's MacBook Pro</p>
+            </div>
+            <div className="flex-1 md:flex-none flex items-center justify-center md:justify-start md:mr-16">
+              <Image src={Mexicoflag} alt="flag" className="rounded-full w-4 h-4 mr-2 inline-block" />
+              <p className="text-gray-400">Mexico City, Mexico&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 month ago&nbsp;&nbsp;&nbsp;&nbsp;</p>
+              <FiTrash2 className='text-lg text-gray-600 ml-6 md:ml-12'/>
+            </div>
+            
+          </div>
         </div>
-        
       </div>
     </div>
   );
-};
+}
 
 export default Settings;
